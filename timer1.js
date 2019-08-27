e6a60d8becbe20ef0bdd92612a3235cac5d29655
Sarah <sarah.mahov@gmail.com>
2019-08-27 21:15:01 +0000

    completed and linted timer1 @CamilaRivera

--- timer1.js
+++ timer1.js
@@ -1,13 +1,13 @@
 const timer = process.argv.slice(2);
-
 const alarm = function(soundType) {
   process.stdout.write(soundType);
 };
-
 timer.forEach((timerInput) => {
-  setTimeout(() => {
-    alarm('\x07');
-  }, timerInput * 1000);
+  if (Number(timerInput) && timerInput >= 0) {
+    setTimeout(() => {
+      alarm('\x07');
+    }, timerInput * 1000);
+  }
 });
 
 
